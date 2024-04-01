5fd2e40c8d51032239728055e82d28c0d04562d2
Arif Hossain <alivearif120@gmail.com>
2024-04-01 11:51:29 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+//import 'package:google_fonts/google_fonts.dart';
+import 'package:meal_app/categories.dart';
+
+void main() {
+  runApp(const MealApp());
+}
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 183, 58, 98),
+  ),
+  
+);
+
+class MealApp extends StatelessWidget {
+  const MealApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: const CategoriesScreen(),
+    );
+  }
+}
