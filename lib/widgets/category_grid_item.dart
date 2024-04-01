856603ea497d058781518f984c5f2b51a54d3e05
Arif Hossain <alivearif120@gmail.com>
2024-04-01 13:33:26 +0200

    Category

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(0.9),
+          ],
+        ),
+      ),
+    );
+  }
+}
