596fa638b0d47d0543ade2940cba3eb8c7bb6b76
Arif Hossain <alivearif120@gmail.com>
2024-04-02 00:18:22 +0200

    Category Item

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/data/data.dart';
-
+import 'package:meal_app/screens/meals.dart';
 import 'package:meal_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const MealsScreen(
+          title: 'Some meals',
+          meals: [],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +33,12 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           //availableCategories.map((category) => CategoryGridItem(category: category)),
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
+            ),
         ],
       ),
     );
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -3,18 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:meal_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key, required this.category});
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () => {},
-      splashColor:Theme.of(context).primaryColor ,
+      splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
       child: Container(
-        padding:const EdgeInsets.all(16),
-        
+        padding: const EdgeInsets.all(16),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
           gradient: LinearGradient(
@@ -29,9 +33,8 @@ class CategoryGridItem extends StatelessWidget {
         child: Text(
           category.title,
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
-            color: Theme.of(context).colorScheme.onBackground,
-          ),
-          
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
         ),
       ),
     );
