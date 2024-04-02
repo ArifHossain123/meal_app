ee4e56651ec3b9a4c13979b62fe8c619f9957c4c
Arif Hossain <alivearif120@gmail.com>
2024-04-02 14:02:05 +0200

    Category Item

---
+++ lib/widgets/meal_iteam.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({
+    super.key,
+    required this.meal,
+  });
+  final Meal meal;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
