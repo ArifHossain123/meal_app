5505f8cd115100f4972cba2ae7343f00d4289426
Arif Hossain <alivearif120@gmail.com>
2024-04-02 14:27:15 +0200

    Category Item

--- lib/widgets/meal_iteam.dart
+++ lib/widgets/meal_iteam.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({
@@ -12,10 +13,43 @@ class MealItem extends StatelessWidget {
     return Card(
       child: InkWell(
         onTap: () {},
-        child: Stack(children: [
-          FadeInImage(placeholder: placeholder, image: image)
-
-        ],),
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(
+                meal.imageUrl,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 6,
+                  horizontal: 6,
+                ),
+                color: Colors.black54,
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text('')
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
