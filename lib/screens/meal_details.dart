4b6043515763f8434c3f227c2f78fc024c6129b4
Arif Hossain <alivearif120@gmail.com>
2024-04-03 21:22:31 +0200

    Category

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -31,15 +31,43 @@ class MealDetailsScreen extends StatelessWidget {
           Text(
             'Ingredient',
             style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold
-                ),
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold),
           ),
-          const SizedBox(height: 14,),
-          for (final ingredient in meal.ingredients) Text('ingredient',
+          const SizedBox(
+            height: 14,
+          ),
+          for (final ingredient in meal.ingredients)
+            Text(
+              ingredient,
               style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                     color: Theme.of(context).colorScheme.onBackground,
                   ),
+            ),
+          const SizedBox(
+            height: 14,
+          ),
+          Text(
+            'Steps',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          for (final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 8,
+              ),
+              child: Text(
+                step,textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
             ),
         ],
       ),
