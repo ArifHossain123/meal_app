1e6dd2165a45e95f4371cae5a2b66d530d496631
Arif Hossain <alivearif120@gmail.com>
2024-04-05 14:45:36 +0200

    BottomNavigationBar

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/screens/categories.dart';
+import 'package:meal_app/screens/meals.dart';
 
 class TabsScreeen extends StatefulWidget {
   const TabsScreeen({super.key});
@@ -9,22 +11,35 @@ class TabsScreeen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreeen> {
-  int _selectedPageIndex=0;
-  void _selectPage(index){
+  int _selectedPageIndex = 0;
+  void _selectPage(int index) {
     setState(() {
-      _selectedPageIndex=index;
+      _selectedPageIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
+    Widget activePage = const CategoriesScreen();
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(title: "Favorite", meals: []);
+    }
     return Scaffold(
-      appBar: AppBar(title: Text(""),),
-      body: ,
-      bottomNavigationBar: BottomNavigationBar(onTap: (index){}, items:const [
-        BottomNavigationBarItem(icon: Icon(Icons.set_meal,), label: 'Category'),
-        BottomNavigationBarItem(icon: Icon(Icons.star), label:"Favorite" ),
-      ],),
+      appBar: AppBar(
+        title: Text(""),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.set_meal,
+              ),
+              label: 'Category'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorite"),
+        ],
+      ),
     );
   }
-  
 }
