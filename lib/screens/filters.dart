d0e16f219806638aaf450fdd569ba1254136ce47
Arif Hossain <alivearif120@gmail.com>
2024-04-09 13:24:29 +0200

    Drawer

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -8,30 +8,39 @@ class FilterScreen extends StatefulWidget {
 }
 
 class _FilterScreenState extends State<FilterScreen> {
+  var _gultenFreeFilterSet = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your Filter'),
+        title:const Text('Your Filter'),
       ),
       body: Column(
         children: [
           SwitchListTile(
-            value: value,
-            onChanged: onChanged,
+            value: _gultenFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _gultenFreeFilterSet = isChecked;
+              });
+            },
             title: Text(
               'Gulten Free',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     color: Theme.of(context).colorScheme.onBackground,
                   ),
             ),
-            subtitle:Text('Only include gluten-free meals',
+            subtitle: Text(
+              'Only include gluten-free meals',
               style: Theme.of(context).textTheme.labelMedium!.copyWith(
                     color: Theme.of(context).colorScheme.onBackground,
                   ),
-            ) ,
+            ),
             activeColor: Theme.of(context).colorScheme.tertiary,
-          contentPadding: EdgeInsets.only(left: 30, right: 22,),
+            contentPadding:const EdgeInsets.only(
+              left: 30,
+              right: 22,
+            ),
           ),
         ],
       ),
